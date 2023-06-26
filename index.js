4651ee3387d094f179bc6075db0e4cdd958b1b99
judy-chelangat <judysigilai75@gmail.com>
2023-06-26 23:21:43 +0300

    declared variables to hold the form and input elements

---
+++ index.js
@@ -0,0 +1,5 @@
+// getting the form  elements in order to do dom manipulation
+
+const form = document.getElementById("search")
+const input = document.getElementById("search-input")
+const searchButton= document.getElementById("search-btn")
