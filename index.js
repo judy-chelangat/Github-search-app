d790c39d023c90096c865793b04225cd84892d06
judy-chelangat <judysigilai75@gmail.com>
2023-06-27 08:21:53 +0300

    added an event listener for the form when submitted

--- index.js
+++ index.js
@@ -3,3 +3,8 @@
 const form = document.getElementById("search")
 const input = document.getElementById("search-input")
 const searchButton= document.getElementById("search-btn")
+
+// adding an eventlistener for the submit form
+form.addEventListener("submit", (e) =>{
+    e.preventDefault() // preventing the form from submiting default
+})
