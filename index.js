19d73d6dd9e5c3442c3941820b035de51e291f0c
judy-chelangat <judysigilai75@gmail.com>
2023-06-27 09:33:06 +0300

    added a variable to hold the search input value

--- index.js
+++ index.js
@@ -4,23 +4,22 @@ const form = document.getElementById("search")
 const input = document.getElementById("search-input")
 const searchButton= document.getElementById("search-btn")
 
-// adding an eventlistener for the submit form
-form.addEventListener("submit", (e) =>{
-    e.preventDefault() // preventing the form from submiting default
-})
 
 const userList=document.getElementById('user-list')
 let repoList=document.getElementById('repos-list')
 
+// adding an eventlistener for the submit form
+form.addEventListener("submit", (e) =>{
+    e.preventDefault() // preventing the form from submiting default
 // fetching the users by name 
+let username =e.target.search.value   /* retrieves the value entered into the search input field when the form is submitted and assigns it to the username variable */
+console.log('searching github')
 
 fetch("https://api.github.com/search/users?q=octocat")
 .then(resp => resp.json())
 .then(data =>{
 
     // using the data from the fetch to display users information
-    
-    let userDetails = document.getElementById("search-user-details")
     data.forEach((character) => {     // using a foreach loop to get each users details
 
         let listItem = document.createElement("li");
@@ -31,4 +30,6 @@ fetch("https://api.github.com/search/users?q=octocat")
 })
 .catch(error => {
     console.log(error)
+})
+
 })
